95827429aa5f1b38240224e23e25f7bb7378c8e0
dareshiri <dareshiri@users.noreply.github.com>
2024-01-24 14:17:20 +0330

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,9 +34,6 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-helloSpeaker.speak();
-byeSpeaker.speak();
-
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
